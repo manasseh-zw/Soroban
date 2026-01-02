f19354099a0495323de2f386b069aa61da457a2a
manasseh-zw <manassehtchanga@gmail.com>
2026-01-02 23:42:34 +0200

    Initial Commit

---
+++ Soroban/SorobanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SorobanApp.swift
+//  Soroban
+//
+//  Created by Manasseh on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SorobanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
